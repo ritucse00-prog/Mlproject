59833e02ad61e3c707b0869a53252c9e7e433b46
ritucse00-prog <ritucse00@gmail.com>
2026-02-02 15:17:28 +0530

    Model_trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,8 @@ from sklearn.model_selection import train_test_split
 
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 from dataclasses import dataclass
 
@@ -52,5 +54,8 @@ if __name__=="__main__":
     train_data,test_data = obj.initiate_data_ingestion()
 
     data_tranformation = DataTransformation()
-    data_tranformation.initiate_transformation(train_data, test_data)
+    train_arr, test_arr, _ = data_tranformation.initiate_transformation(train_data, test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
 
--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -96,7 +96,7 @@ class DataTransformation:
                 input_feature_train_arr, np.array(target_feature_train_df)
             ]
             test_arr = np.c_[
-                input_feature_test_df,np.array(target_feature_test_df)
+                input_feature_test_arr,np.array(target_feature_test_df)
             ]
 
             logging.info("Saved preprocessing object.")
